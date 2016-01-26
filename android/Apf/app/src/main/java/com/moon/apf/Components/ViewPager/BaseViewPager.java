b18bcf1ad9d5b38c0bada5b6a8a49fb731c2e1da
moon <mjy@add2paper.com>
2016-01-26 16:26:28 +0900

    android - modify parent views get view method

--- android/Apf/app/src/main/java/com/moon/apf/Components/ViewPager/BaseViewPager.java
+++ android/Apf/app/src/main/java/com/moon/apf/Components/ViewPager/BaseViewPager.java
@@ -1,13 +1,11 @@
 package com.moon.apf.Components.ViewPager;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
+import com.moon.apf.Components.ChildViewInterface;
 import com.moon.apf.Components.ParentViewInterface;
 import com.moon.apf.R;
 
@@ -20,15 +18,14 @@ import java.util.Iterator;
 public class BaseViewPager implements BaseViewPagerInterface, ParentViewInterface {
 
     public Context mContext;
+    public ArrayList<ChildViewInterface> views;
 
     public ViewPager mViewPager;
     public ViewGroup.LayoutParams mParams;
 
-    private ArrayList<View> mPageViews;
-
     public BaseViewPager(Context context){
 
-        mPageViews = new ArrayList<>();
+        views = new ArrayList<>();
 
         mContext = context;
         mViewPager = new ViewPager(mContext);
@@ -37,16 +34,16 @@ public class BaseViewPager implements BaseViewPagerInterface, ParentViewInterfac
     }
 
     @Override
-    public void addPageView(View view) {
-        mPageViews.add(view);
-        mViewPager.addView(view);
+    public void addPageView(ChildViewInterface view) {
+        views.add(view);
     }
 
     @Override
     public ViewPager getView() {
-        Iterator<View> pageViews = mPageViews.iterator();
+        Iterator<ChildViewInterface> pageViews = views.iterator();
         while(pageViews.hasNext()){
-            mViewPager.addView(pageViews.next());
+            View view = pageViews.next().getView();
+            mViewPager.addView(view);
         }
 
         return mViewPager;
--- android/Apf/app/src/main/java/com/moon/apf/Components/ViewPager/BaseViewPagerInterface.java
+++ android/Apf/app/src/main/java/com/moon/apf/Components/ViewPager/BaseViewPagerInterface.java
@@ -3,11 +3,11 @@ package com.moon.apf.Components.ViewPager;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.moon.apf.Components.ChildViewInterface;
+
 /**
  * Created by moon on 2016. 1. 26..
  */
 public interface BaseViewPagerInterface {
-
-    ViewPager getView();
-    void addPageView(View view);
+    void addPageView(ChildViewInterface view);
 }
