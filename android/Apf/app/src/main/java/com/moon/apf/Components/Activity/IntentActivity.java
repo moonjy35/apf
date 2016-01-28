72e6f36544eeca39d8be153b7dbbd28fe6a80542
moon <mjy@add2paper.com>
2016-01-28 18:11:37 +0900

    android : test activity layout from javascript

--- android/Apf/app/src/main/java/com/moon/apf/Components/Activity/IntentActivity.java
+++ android/Apf/app/src/main/java/com/moon/apf/Components/Activity/IntentActivity.java
@@ -2,9 +2,12 @@ package com.moon.apf.Components.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.view.View;
 
 import com.moon.apf.Components.DefaultComponentInterface;
+import com.moon.apf.LayoutFactory.LayoutFactory;
 import com.moon.apf.LayoutInflaters.IntentLayoutInflater;
 
 /**
@@ -19,18 +22,26 @@ public class IntentActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mIntent = getIntent();
 
-        IntentLayoutInflater inflater = new IntentLayoutInflater(this, mIntent);
-        mComponent = inflater.getComponent();
+        //TODO 디비 상에 저장된 정보로 불러오게끔 변경되어야함
+        View view = LayoutFactory.getView(1);
+        _ContentLayout.addView(view);
 
-        if(mComponent != null){
-            Log.d("", "INFALTE FROM INTENT INFLATER");
-            _ContentLayout.addView(mComponent.getView());
-        }
-        else
-        {
-            Log.d("", "THERE IS NO MEMORIZING LAYOUT");
-        }
+
+
+        //인텐트로 불러옴
+//        mIntent = getIntent();
+//
+//        IntentLayoutInflater inflater = new IntentLayoutInflater(this, mIntent);
+//        mComponent = inflater.getComponent();
+//
+//        if(mComponent != null){
+//            Log.d("", "INFALTE FROM INTENT INFLATER");
+//            _ContentLayout.addView(mComponent.getView());
+//        }
+//        else
+//        {
+//            Log.d("", "THERE IS NO MEMORIZING LAYOUT");
+//        }
     }
 }
