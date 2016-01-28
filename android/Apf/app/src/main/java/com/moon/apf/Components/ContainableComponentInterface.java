6bd44e247002ce34257c789f045525eef93d7764
moon <mjy@add2paper.com>
2016-01-28 18:09:55 +0900

    android : add missing interface

--- android/Apf/app/src/main/java/com/moon/apf/Components/ContainableComponentInterface.java
+++ android/Apf/app/src/main/java/com/moon/apf/Components/ContainableComponentInterface.java
@@ -1,8 +1,11 @@
 package com.moon.apf.Components;
 
+import android.view.View;
+
 /**
  * Created by moon on 2016. 1. 26..
  */
 public interface ContainableComponentInterface {
     DefaultComponentInterface addView(DefaultComponentInterface view);
+    View getView();
 }
