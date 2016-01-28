c5c616430fa216b8e2c0f32eb7dbe4abc94d50dc
moon <mjy@add2paper.com>
2016-01-28 18:11:15 +0900

    html : add simple layout test

--- html/script.js
+++ html/script.js
@@ -5,8 +5,11 @@
 	}
 
 	$(document).ready(function(){
-		$('#button').on('click', function(){
+		$('#test').on('click', function(){
 			WebApp.test();
+		});
+		$('#button').on('click', function(){
+			
 			WebApp.newActivity('http://10.10.10.235:6871/test.html', '테스트');
 			//changeIframeSrc('http://www.daum.net');
 		});
